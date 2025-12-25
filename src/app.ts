466f27bd6caa65835d220d87e8052b771664c2d4
gabebonham <grotegabriel@gmail.com>
2025-12-25 00:17:41 -0300

    configured mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express'
-
+import User from './config/database/schemas/user.schema'
 const app = express()
 
 app.use(express.json())
-app.post('/name', (req, res) => {
-  const { name } = req.body
-  res.send({ nameSent: name })
+app.post('/users', async (req, res) => {
+  const user = await User.create(req.body)
+  return res.status(201).json(user)
+})
+app.get('/users', async (req, res) => {
+  const users = await User.find()
+  return res.json(users)
 })
 export default app
---
+++ src/config/database/index.ts
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose'
+
+export function connectDB() {
+  mongoose
+    .connect(process.env.MONGO_URI!)
+    .then(() => console.log('MongoDB conectado'))
+    .catch((err) => console.error(err))
+}
