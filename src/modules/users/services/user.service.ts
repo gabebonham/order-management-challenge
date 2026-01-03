580233aa88ba87ccbf6529db8012ffddcc433333
gabebonham <grotegabriel@gmail.com>
2026-01-02 22:18:26 -0300

    added email validation

--- src/modules/users/services/user.service.ts
+++ src/modules/users/services/user.service.ts
@@ -3,6 +3,10 @@ import { CreateUserDTO, createUserSchema } from '../dtos/create-user.dto'
 import User, { IUser } from '../models/user.model'
 
 export const createUser = async (data: CreateUserDTO): Promise<IUser> => {
+  const existingUser = await User.findOne({ email: data.email })
+  if (existingUser) {
+    throw new CustomError('User already exists', 400)
+  }
   return await User.create({ email: data.email, password: data.password })
 }
 export const getUsers = async (): Promise<IUser[]> => {
